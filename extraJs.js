535f13001e60e2d917d7d13f76c7239f410eeb79
Fredrik Olsen Kapoun <fredrik99k@hotmail.com>
2024-02-20 15:09:51 +0100

    test

---
+++ extraJs.js
@@ -0,0 +1,17 @@
+// Create a new div element
+var newDiv = document.createElement("div");
+
+newDiv.id = "GlupHjelpBox";
+
+newDiv.innerHTML = `
+  <p>Problemer med å logge inn?</p>
+  <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å gå til Glup sitt kunnskapssenter</a>
+  <p>Lurer du på hva Glup er?</p>
+  <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å lese mer om læringsplatformen Glup</a>
+`;
+
+var referenceNode = document.querySelector(".form-footer.external-providers");
+
+var parentNode = referenceNode.parentNode;
+
+parentNode.insertBefore(newDiv, referenceNode);
