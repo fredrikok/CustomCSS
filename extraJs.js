5cdb6d794777af129b15240b251a0f7bf3c7a70d
fredrikok <fredrik99k@hotmail.com>
2024-02-29 09:43:08 +0100

    Test

--- extraJs.js
+++ extraJs.js
@@ -1,20 +1,24 @@
-// Create a new div element
-var newDiv = document.createElement("div");
+//----------------------------------------------hjelpebox------------------------------------------------------------------
 
-newDiv.id = "GlupHjelpBox";
+document.addEventListener("DOMContentLoaded", function () {
+  var newDiv = document.createElement("div");
+  newDiv.id = "GlupHjelpBox";
+  newDiv.innerHTML = `
+    <p class="hjelpeBoxP">Problemer med å logge inn?</p>
+    <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å gå til Glup sitt kunnskapssenter</a>
+    <p class="hjelpeBoxP">Lurer du på hva Glup er?</p>
+    <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å lese mer om læringsplatformen Glup</a>
+  `;
 
-newDiv.innerHTML = `
-  <p class="hjelpeBoxP">Problemer med å logge inn?</p>
-  <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å gå til Glup sitt kunnskapssenter</a>
-  <p class="hjelpeBoxP">Lurer du på hva Glup er?</p>
-  <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å lese mer om læringsplatformen Glup</a>
-`;
+  var referenceNode = document.querySelector(".form-footer");
 
-var referenceNode = document.querySelector(".form-footer.external-providers");
-
-var parentNode = referenceNode.parentNode;
-
-parentNode.insertAfter(newDiv, referenceNode);
+  if (referenceNode) {
+    referenceNode.parentNode.insertBefore(newDiv, referenceNode.nextSibling);
+  } else {
+    console.log("The reference .form-footer div was not found.");
+  }
+});
+//------------------------------------------------Bakgrunn------------------------------------------------------------------------------
 
 document.addEventListener("DOMContentLoaded", function () {
   var backgroundDiv = document.createElement("div");
