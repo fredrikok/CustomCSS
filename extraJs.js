5f0aeb4bd29369eaa06567c6834653a96f21a5d9
fredrikok <fredrik99k@hotmail.com>
2024-02-29 08:42:53 +0100

    update

--- extraJs.js
+++ extraJs.js
@@ -4,9 +4,9 @@ var newDiv = document.createElement("div");
 newDiv.id = "GlupHjelpBox";
 
 newDiv.innerHTML = `
-  <p>Problemer med å logge inn?</p>
+  <p class="hjelpeBoxP">Problemer med å logge inn?</p>
   <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å gå til Glup sitt kunnskapssenter</a>
-  <p>Lurer du på hva Glup er?</p>
+  <p class="hjelpeBoxP">Lurer du på hva Glup er?</p>
   <a href="https://www.smartskill.no/glup/kom-i-gang" target="_blank" rel="noreferrer">Klikk her for å lese mer om læringsplatformen Glup</a>
 `;
 
